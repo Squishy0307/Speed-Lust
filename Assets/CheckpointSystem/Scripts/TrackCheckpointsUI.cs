e5368f2db1ba386f0e6b55e10de8e611fad34294
OwzyBrand <dylanmaclaren@live.com>
2023-10-25 10:55:45 -0400

    Checkpoint v#

--- Assets/CheckpointSystem/Scripts/TrackCheckpointsUI.cs
+++ Assets/CheckpointSystem/Scripts/TrackCheckpointsUI.cs
@@ -1,36 +1,32 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckpointUI : MonoBehaviour
-{
+public class TrackCheckpointsUI : MonoBehaviour {
+
     [SerializeField] private TrackCheckpoints trackCheckpoints;
 
-    private void Start()
-    {
+    private void Start() {
         trackCheckpoints.OnPlayerCorrectCheckpoint += TrackCheckpoints_OnPlayerCorrectCheckpoint;
         trackCheckpoints.OnPlayerWrongCheckpoint += TrackCheckpoints_OnPlayerWrongCheckpoint;
 
         Hide();
     }
 
-    private void TrackCheckpoints_OnPlayerCorrectCheckpoint(object sender, System.EventArgs e)
-    {
-        Hide();
-    }
-
-    private void TrackCheckpoints_OnPlayerWrongCheckpoint(object sender, System.EventArgs e)
-    {
+    private void TrackCheckpoints_OnPlayerWrongCheckpoint(object sender, System.EventArgs e) {
         Show();
     }
 
-    private void Show()
-    {
+    private void TrackCheckpoints_OnPlayerCorrectCheckpoint(object sender, System.EventArgs e) {
+        Hide();
+    }
+
+    private void Show() {
         gameObject.SetActive(true);
     }
 
-    private void Hide()
-    {
+    private void Hide() {
         gameObject.SetActive(false);
     }
+
 }
