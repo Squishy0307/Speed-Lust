00feeb2a32500d64393fa96faa51fff86df2c1b0
Squishy0307 <38110101+Squishy0307@users.noreply.github.com>
2023-11-14 17:56:07 -0500

    Scene Loader and Fader

--- Assets/_Scripts/ShipHUD.cs
+++ Assets/_Scripts/ShipHUD.cs
@@ -14,7 +14,7 @@ public class ShipHUD : MonoBehaviour
     
     private int checkpointCount;                    //Total Checkpoints the vehicle has went through
     private Transform nextCheckpoint;               //Next checkpoint 
-    private Transform previousCheckpoint;           //Previous checkpoint
+    public Transform previousCheckpoint;           //Previous checkpoint ------------[SET PRIVATE LATER]------------
 
     private VehicleMovement vehicle;                //Reference to the vehcile script
 
@@ -44,6 +44,11 @@ public class ShipHUD : MonoBehaviour
 
         speed = vehicle.GetCurrentSpeed();
         UpdateHUD();
+
+        if(Input.GetKeyDown(KeyCode.F9)) //-----------[Remove Later]-----------
+        {
+            StartCoroutine(Test());
+        }
 	}
 
     void UpdateHUD()
@@ -69,7 +74,20 @@ public class ShipHUD : MonoBehaviour
 
     public void Respawn()
     {
-        transform.position = previousCheckpoint.position + new Vector3(0, -5, 0);
+        if (previousCheckpoint != null)
+        {
+            transform.position = previousCheckpoint.position + new Vector3(0, -5, 0);
+        }
+        else
+        {
+            Debug.LogError("PREVIOUS CHECKPOINNT IS NULL!");
+        }
+    }
+
+    IEnumerator Test()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Respawn();
     }
 
     public void UpdateCheckpointCount()
