0d493c5268e66e549392c7ba044d73ce70f4b616
OwzyBrand <dylanmaclaren@live.com>
2023-11-27 23:45:00 -0500

    Speedometer
    Added the Speedometer!

---
+++ Assets/_Scripts/SpeedHUD.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedHUD : MonoBehaviour
+{
+    private VehicleMovement vehicleMovement;
+    public GameObject player;
+    public Slider speedSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vehicleMovement = player.GetComponent<VehicleMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedSlider.value = vehicleMovement.speed;
+    }
+}
