6336370c03792cc0c076033aeb748098260c00cb
Rohan Patel <38110101+Squishy0307@users.noreply.github.com>
2023-12-04 10:58:19 -0500

    Added ground grass texture and fixed mega boost
    mega boost material will only show effects when player is in 3rd lap.

--- Assets/_Scripts/MegaBoost.cs
+++ Assets/_Scripts/MegaBoost.cs
@@ -6,13 +6,56 @@ public class MegaBoost : MonoBehaviour
 {
     [SerializeField] float boostDuration = 10f;
     [SerializeField] float speedIncreaseRate = 1f;
+    [SerializeField] private Material portalMat;
+    [SerializeField] private Material innerRingMat;
+    [SerializeField] private Material outerRingMat;
+    [SerializeField] private Material orangeRingMat;
+
+    private bool EffectsEnabled = false;
+
+    private void Start()
+    {
+        orangeRingMat.SetFloat("_Speed", 0);
+        outerRingMat.SetFloat("_Speed", 0);
+        innerRingMat.SetFloat("_Speed", 0);
+
+        portalMat.SetFloat("_InnerFade", 1f);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out VehicleMovement vehicle))
         {
-            Debug.Log("<b><color=green> MEGA BOOST!!</color></b>");
-            vehicle.MegaBoostInitiated(boostDuration,speedIncreaseRate);
+
+            if (vehicle.lapNumber == "Lap 2" && vehicle.CompareTag("Player"))
+            {
+                StartCoroutine(enableEffects());
+            }
+
+            else if (vehicle.lapNumber == "Lap 3")
+            {
+                Debug.Log("<b><color=green> MEGA BOOST!!</color></b>");
+
+                //TO-DO: HIDE ALL UI 
+
+                vehicle.MegaBoostInitiated(boostDuration, speedIncreaseRate);
+            }
+        }
+    }
+
+    IEnumerator enableEffects()
+    {
+        if(!EffectsEnabled)
+        {
+            EffectsEnabled = true;
+
+            yield return new WaitForSeconds(1f);
+
+            orangeRingMat.SetFloat("_Speed", 1);
+            outerRingMat.SetFloat("_Speed", 0.5f);
+            innerRingMat.SetFloat("_Speed", 0.5f);
+
+            portalMat.SetFloat("_InnerFade", 0.67f);
         }
     }
 }
